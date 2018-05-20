c547d6ba457e53637057c7fabcd08e06aa0cb855
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2018-05-20 08:44:02 +0200

    refactoring done

--- bartek/get_start_codon_and_SD.py
+++ bartek/get_start_codon_and_SD.py
@@ -5,17 +5,18 @@ def get_start_codon_and_SD (genome):
     full_genome = fp.read()
     fp.close()
 
-    x = re.findall(".{10}TAC", full_genome)
-    b=[]
-    for z in x:
-        v = re.match("[ACTG]*(TCCTCC|TCCTTC|TCCTC|TCCTT)[ACTG]*", z)
-        if v != None:
-            b.append(v.string)
-    z = []
-    for w in b:
-        x = [m.start() for m in re.finditer(w, full_genome)]
-        z.extend(x)
-    z = set(z)
-    for q in z:
+    ten_and_start = re.findall(".{10}TAC", full_genome)
+    ready_sequences_to_search=[]
+    for match in ten_and_start:
+        supplement_match = re.match("[ACTG]*(TCCTCC|TCCTTC|TCCTC|TCCTT)[ACTG]*", match)
+        if supplement_match != None:
+            ready_sequences_to_search.append(supplement_match.string)
+    positions = []
+    for sequence in ready_sequences_to_search:
+        position = [m.start() for m in re.finditer(sequence, full_genome)]
+        positions.extend(position)
+    positions = set(positions)
+    for q in positions:
+        #position have to start from codon start
         q = q + 13
-    return z
+    return positions
