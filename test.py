73a236495bb271445945751a7c271291e72f4fd4
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2018-05-20 03:46:43 +0200

    dascsc

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ fp = open("/home/panda/Pulpit/sequence.fasta", "r")
 full_genome = fp.read()
 fp.close()
 
-startCodonsWith10rand = re.findall(".{10}TAC", full_genome)
-templist = []
-for z in startCodonsWith10rand:
-    match = re.match("[ACTG]*(TCCTCC|TCCTTC|TCCTC|TCCTT)[ACTG]*", z)
-    if match != None:
-        templist.append(match.string)
-positions = []
-for w in templist:
+x = re.findall(".{10}TAC", full_genome)
+b=[]
+for z in x:
+    v = re.match("[ACTG]*(TCCTCC|TCCTTC|TCCTC|TCCTT)[ACTG]*", z)
+    if v != None:
+        b.append(v.string)
+z = []
+for w in b:
     x = [m.start() for m in re.finditer(w, full_genome)]
-    z.extend(x + len(w))
+    z.extend(x)
 print(set(z))
